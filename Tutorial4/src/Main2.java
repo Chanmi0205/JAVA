219f0c7143b6e87b21f5c4c3654c83c82783a49e
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-08-16 00:45:07 +0900

    Add files via upload

---
+++ Tutorial4/src/Main2.java
@@ -0,0 +1,18 @@
+
+public class Main2 {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		System.out.println("현재의 a는  " + a + "입니다.");
+		System.out.println("\n");
+		
+		a++; // ++ : 증감연산자
+		
+		System.out.println("현재의 a는  " + a + "입니다.");
+		System.out.println("현재의 a는  " + ++a + "입니다."); //더한 다음 출력함
+		System.out.println("현재의 a는  " + a++ + "입니다."); //출력한 후 더함
+		System.out.println("현재의 a는  " + a + "입니다.");
+	}
+
+}
---
+++ Tutorial4/src/Main6.java
@@ -0,0 +1,10 @@
+
+public class Main6 {
+	
+	public static void main(String[] args) {
+		
+		double a = Math.pow(3.0, 20.0);
+		System.out.println("3의 20제곱은 " + (int)a + "입니다. ");
+	}
+
+}
