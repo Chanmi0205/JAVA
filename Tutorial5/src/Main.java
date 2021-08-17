b7656d49bc72ebdf56216fecfa8d8495b266b3b8
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-08-17 14:05:57 +0900

    Add files via upload

---
+++ Tutorial5/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	 String a = "I Love you";
+	 if ( a.contains("Love")) { //contains : 자바가 제공하는 함수
+		 
+		 //포함하는 경우 실행되는 부분
+		 System.out.println("Me Too.");
+		 
+	 }
+	 
+	 else {
+		 //포함하지 않는 경우 실행되는 부분
+		 System.out.println("I Hate You.");
+	 }
+		 
+	 }
+}
