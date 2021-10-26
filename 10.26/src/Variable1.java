a80d97c32de1194e8d3b45070dee331d9a4b560d
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-10-26 19:47:08 +0900

    Add files via upload

---
+++ 10.26/src/Variable1.java
@@ -0,0 +1,29 @@
+
+public class Variable1 {
+	
+	public static void main(String[] args) {
+		int level;  // 정수형 변수 level을 선언
+		level = 10; // level 변수에 값 10을 대입
+		System.out.println("현재 레벨은 : "+ level); // level값 출력
+		
+		int number1 = 100;
+		double number2 = 116.3324;
+		int number3 = (int) (number1 + number2);
+		
+		int number4 = 100;
+		double number5 = 116.3324;
+		
+		System.out.println(number3);
+		System.out.println(number4 + number5);
+		
+		/* 참 거짓 데이터 : boolean
+
+		 * 정수 데이터 = int
+
+		 * 실수 데이터 : double
+
+		 * 문자열 데이터 = String  */
+		
+	}
+
+}
