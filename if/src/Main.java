828218c51b1a57f046e98964af72d73b52ed60cd
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-10-31 20:50:12 +0900

    Add files via upload

---
+++ if/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int b, c;
+		
+		for ( b=2; b<=9; b++ ) {
+			for ( c=1; c<=9; c++ ) {
+				System.out.println( b + " * " + c + " = " + b*c);
+			}
+	}
+		
+	}
+
+}
