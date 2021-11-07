4fc24923fda73004cebe726881a275d56ccb9e6b
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-11-07 20:37:05 +0900

    Add files via upload

---
+++ constructor/src/constructor/Person.java
@@ -0,0 +1,12 @@
+package constructor;
+
+public class Person {
+	String name;
+	int studentID;
+	
+	public Person(String pname) {
+		name = pname;
+		System.out.println(pname);
+	}
+
+}
---
+++ constructor/src/constructor/Student.java
@@ -0,0 +1,10 @@
+package constructor;
+
+public class Student {
+	int studentID;
+	
+	public Student(int studentID) {
+		this.studentID = studentID;
+		System.out.println(studentID);
+	}
+}
