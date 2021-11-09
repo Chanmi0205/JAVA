ed1b681583634b3555f614858673c4628def2667
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2021-11-09 17:45:17 +0900

    Add files via upload

---
+++ 11.09/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Student studentAhn = new Student();  
+//		studentAhn.studentName = "¾È¿¬¼ö";
+		
+//		System.out.println(studentAhn.studentName);
+		System.out.println(studentAhn.getStudentName());
+		
+		
+	}
+
+}
---
+++ 11.09/src/Student.java
@@ -0,0 +1,17 @@
+
+public class Student {
+	
+	int studentID;
+	private String studentName = "¹ÚÂù¹Ì";
+	int grade;
+	String address;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+}
