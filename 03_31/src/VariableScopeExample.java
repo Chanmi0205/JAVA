cf35e82800a20ebaac16c0790937222f35cbd20d
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2022-03-31 18:13:16 +0900

    Add files via upload

---
+++ 03_31/src/VariableScopeExample.java
@@ -0,0 +1,17 @@
+public class VariableScopeExample {
+	
+	public static void main(String[] args) {
+		
+		// Swap
+		int x = 3;
+		int y = 5;
+		System.out.println("x : " + x + ", y : " + y);
+		
+		int temp = x;
+		x = y;
+		y = temp;
+		System.out.println("x : " + x + ", y : " + y);
+		
+	}
+
+}
