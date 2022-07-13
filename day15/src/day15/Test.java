02109ba0b060155240ebc61d67e56500ebe4b674
Chanmi0205 <84673395+Chanmi0205@users.noreply.github.com>
2022-07-13 22:45:12 +0900

    Add files via upload

---
+++ day15/src/day15/Test.java
@@ -0,0 +1,19 @@
+package day15;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		int a, b = 10;
+		a = b;
+		b = 11;
+		System.out.println("b 변경 후 바뀐 값 : "+a);
+		
+		int arr[] = new int[3];
+		int arr2[] = new int[3];
+		
+		arr = {1, 2, 3};
+		
+		
+	}
+
+}
